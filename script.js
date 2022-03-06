2b6366bf68335a7e52cc9b16db4dadbdd2c29454
Leda <ledathemis@protonmail.com>
2022-03-06 14:02:32 +0300

    add operate() function

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+function operate(op, a, b) {
+  switch (op) {
+    case "+":
+      add(a, b);
+      break;
+    case "-":
+      subtract(a, b);
+      break;
+    case "*":
+      multiply(a, b);
+      break;
+    case "/":
+      divide(a, b);
+      break;
+  }
+}
+
 // ARITHMETIC FUNCTIONS
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
