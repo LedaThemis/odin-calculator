ba41b6eb6d5964aafd85ffbb4835beb3017f318e
Leda <ledathemis@protonmail.com>
2022-03-07 09:04:42 +0300

    add operation handling logic

--- script.js
+++ script.js
@@ -13,7 +13,25 @@ digits.forEach((digit) => digit.addEventListener("click", handleDigitClick));
 const operations = document.querySelectorAll(".operation");
 operations.forEach((op) => op.addEventListener("click", handleOperationClick));
 
-function handleOperationClick(e) {}
+function handleOperationClick(e) {
+  if (displayValue.first === "") return;
+  if (displayValue.first !== "" && displayValue.second !== "") {
+    displayValue.first = `${operate(
+      displayValue.op,
+      displayValue.first,
+      displayValue.second
+    )}`;
+
+    displayValue.op = e.target.textContent;
+    displayValue.second = "";
+
+    updateDisplay(displayValue);
+  }
+  if (displayValue.first !== "" && displayValue.second === "") {
+    displayValue.op = e.target.textContent;
+    updateDisplay(displayValue);
+  }
+}
 
 function handleDigitClick(e) {
   if (displayValue.op === "") {
