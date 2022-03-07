5f0228c893945dee850125ef08c5744af598a43a
Leda <ledathemis@protonmail.com>
2022-03-07 08:54:13 +0300

    edit updateDisplay, handleDigit

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 import operate from "./operate.js";
 
-let displayValue = "";
+let displayValue = { first: "", op: "", second: "" };
+let firstNumber = "";
+let secondNumber = "";
+let currentOp = "";
 
 const DISPLAY_TEXT = document.querySelector("#display-text");
 
 const digits = document.querySelectorAll(".digit");
 digits.forEach((digit) => digit.addEventListener("click", handleDigitClick));
 
+const operations = document.querySelectorAll(".operation");
+operations.forEach((op) => op.addEventListener("click", handleOperationClick));
+
+function handleOperationClick(e) {}
+
 function handleDigitClick(e) {
-  displayValue += e.target.textContent;
+  if (displayValue.op === "") {
+    displayValue.first += e.target.textContent;
+  } else {
+    displayValue.second += e.target.textContent;
+  }
   updateDisplay(displayValue);
 }
 
-function updateDisplay(value) {
-  DISPLAY_TEXT.textContent = value;
+function updateDisplay(v) {
+  DISPLAY_TEXT.textContent = v.first + v.op + v.second;
 }
