ff5648817c83ba7af2195a058e29f2f0f781d4be
Leda <ledathemis@protonmail.com>
2022-03-07 08:22:13 +0300

    move operate() to a separate file

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-function operate(op, a, b) {
-  switch (op) {
-    case "+":
-      add(a, b);
-      break;
-    case "-":
-      subtract(a, b);
-      break;
-    case "*":
-      multiply(a, b);
-      break;
-    case "/":
-      divide(a, b);
-      break;
-  }
-}
+import operate from "./operate.js";
 
-// ARITHMETIC FUNCTIONS
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+console.log(operate("+", 1, 1));
