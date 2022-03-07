933327ea6bac36b19ca1bb9b374b2b12e81cb736
Leda <ledathemis@protonmail.com>
2022-03-07 08:33:43 +0300

    add function to update display

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ import operate from "./operate.js";
 
 let displayValue = "";
 
+const DISPLAY_TEXT = document.querySelector("#display-text");
+
 const digits = document.querySelectorAll(".digit");
 digits.forEach((digit) => digit.addEventListener("click", handleDigitClick));
 
 function handleDigitClick(e) {
   displayValue += e.target.textContent;
+  updateDisplay(displayValue);
+}
+
+function updateDisplay(value) {
+  DISPLAY_TEXT.textContent = value;
 }
